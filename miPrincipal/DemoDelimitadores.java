bdf544a8688c36da9280f4730d37a543229feb53
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2024-03-15 16:43:03 +0000

    Listo

--- miPrincipal/DemoDelimitadores.java
+++ miPrincipal/DemoDelimitadores.java
@@ -7,13 +7,26 @@ public class DemoDelimitadores {
         System.out.println("************************");
         System.out.println();
         Delimitadores objDel = new Delimitadores();
-        String expr = "(a+b)/2";
-        if(objDel.evaluacion(expr)){
-            System.out.println("Expresion correcta");
-        }
-        else{
-            System.out.println("Expresion Inceorrecta");
-        }
+        //String expr = "(a+b)/2";
+         
+        String expr= "while (m<(n[8]+o)) { "+
+                     "int p=7; "+
+                     "/*comentarios*/"+
+                     "               } ";
+        if (objDel.evaluacion(expr))
+           System.out.println("Expresión correcta");
+        else
+           System.out.println("Expresión Incorrecta");
+        expr= "while (m<(n[8]+o)) { "+
+              "int p=7; "+
+              "/*comentarios*/";
+        if (objDel.evaluacion(expr))
+            System.out.println("Expresión correcta");
+        else
+            System.out.println("Expresión Incorrecta");
+            
+
+        
         
 
     }
